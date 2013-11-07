49d456de0a7fbc2f33fd049b519aa17276c419fb
Michal Fojtik <mfojtik@redhat.com>
2013-11-07 23:10:29 +0100

    Added list of deployments

--- usr/app/lib/application.rb
+++ usr/app/lib/application.rb
@@ -5,15 +5,21 @@ class DeploymentsApp < Sinatra::Base
 
   set :root, File.join(File.dirname(__FILE__), '..')
 
+  helpers do
+
+    def deployments
+      Dir.glob(ENV['OPENSHIFT_DEPLOYMENTS_DIR'] + '*').reject { |f|
+        File.basename(f) !~ /^(\d{4})\-/ || !File.exists?(File.join(f, 'metadata.json'))
+      }.inject({}) { |result, f|
+        result[File.basename(f)] = JSON::parse(File.read(File.join(f, 'metadata.json')))
+        result
+      }
+    end
+
+  end
+
   get '/' do
-    deployments = Dir.glob(ENV['OPENSHIFT_DEPLOYMENTS_DIR'] + '*').inject({}) { |result, f|
-      next unless File.basename(f) =~ /^(\d{4})\-/
-      next unless File.exists?(File.join(f, 'metadata.json'))
-      result[File.basename(f)] = JSON::parse(File.read(File.join(f, 'metadata.json')))
-      result
-    }
-    puts deployments.inspect
-    erb :index, :locals => { :deployments => deployments }
+    erb :index
   end
 
 end
