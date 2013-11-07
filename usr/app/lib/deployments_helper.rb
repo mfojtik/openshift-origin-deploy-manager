dbbdfe9b831dabe49b1323f4184c7e7f476cf540
Michal Fojtik <mfojtik@redhat.com>
2013-11-08 00:13:42 +0100

    Added list of deployments

--- usr/app/lib/deployments_helper.rb
+++ usr/app/lib/deployments_helper.rb
@@ -3,7 +3,7 @@ module DeploymentsHelper
   def deployments
     Dir.glob(ENV['OPENSHIFT_DEPLOYMENTS_DIR'] + '*').reject { |f|
       File.basename(f) !~ /^(\d{4})\-/ || !File.exists?(File.join(f, 'metadata.json'))
-    }.sort.inject({}) { |result, f|
+    }.sort.reverse.inject({}) { |result, f|
       result[File.basename(f)] = JSON::parse(File.read(File.join(f, 'metadata.json')))
       result
     }
