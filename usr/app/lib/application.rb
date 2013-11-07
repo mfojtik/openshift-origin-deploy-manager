fef75fc5d38ec89b4c03cf4cca83dc6ddc9b1184
Michal Fojtik <mfojtik@redhat.com>
2013-11-07 22:48:33 +0100

    Added list of deployments

--- usr/app/lib/application.rb
+++ usr/app/lib/application.rb
@@ -1,11 +1,17 @@
 require 'sinatra/base'
+require 'json'
 
 class DeploymentsApp < Sinatra::Base
 
   set :root, File.join(File.dirname(__FILE__), '..')
 
   get '/' do
-    erb :index
+    deployments = Dir.foreach(ENV['OPENSHIFT_DEPLOYMENTS_DIR']).inject({}) { |result, f|
+      next unless File.exists?(File.join(f, 'metadata.json'))
+      result[File.basename(f)] = JSON::parse(File.read(File.join(f, 'metadata.json')))
+      result
+    }
+    erb :index, :locals => { :deployments => deployments }
   end
 
 end
