c7cdc1a774e8896f5d5e258856063e8d496cca88
Michal Fojtik <mfojtik@redhat.com>
2013-11-07 23:53:12 +0100

    Added list of deployments

--- usr/app/lib/application.rb
+++ usr/app/lib/application.rb
@@ -1,22 +1,11 @@
 require 'sinatra/base'
 require 'json'
+require_relative './deployments_helper'
 
 class DeploymentsApp < Sinatra::Base
 
   set :root, File.join(File.dirname(__FILE__), '..')
-
-  helpers do
-
-    def deployments
-      Dir.glob(ENV['OPENSHIFT_DEPLOYMENTS_DIR'] + '*').reject { |f|
-        File.basename(f) !~ /^(\d{4})\-/ || !File.exists?(File.join(f, 'metadata.json'))
-      }.inject({}) { |result, f|
-        result[File.basename(f)] = JSON::parse(File.read(File.join(f, 'metadata.json')))
-        result
-      }
-    end
-
-  end
+  helpers DeploymentsHelper
 
   get '/' do
     erb :index
