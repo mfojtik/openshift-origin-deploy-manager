4f09054417cc98326a2d4b3e0a2f17d6028c717a
Michal Fojtik <mfojtik@redhat.com>
2013-11-07 23:58:55 +0100

    Added helper file

---
+++ usr/app/lib/deployments_helper.rb
@@ -0,0 +1,28 @@
+module DeploymentsHelper
+
+  def deployments
+    Dir.glob(ENV['OPENSHIFT_DEPLOYMENTS_DIR'] + '*').reject { |f|
+      File.basename(f) !~ /^(\d{4})\-/ || !File.exists?(File.join(f, 'metadata.json'))
+    }.inject({}) { |result, f|
+      result[File.basename(f)] = JSON::parse(File.read(File.join(f, 'metadata.json')))
+      result
+    }
+  end
+
+  def commit(commit_id)
+    Dir.chdir(File.join(ENV['OPENSHIFT_HOMEDIR'], 'git', "#{ENV['OPENSHIFT_APP_NAME']}.git")) do
+      parse_git_commit %x[git show -s --pretty=full #{commit_id}]
+    end
+  end
+
+  private
+
+  def parse_git_commit(commit)
+    data = commit.split("\n")
+    {
+      'author' => data[1].split(" ")[1],
+      'message' => data[3..-1].map { |m| m.strip }.join("\n")
+    }
+  end
+
+end
