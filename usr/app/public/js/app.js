2587ca42235547e3ed0892528c7c072bcddaa2d2
Michal Fojtik <mfojtik@redhat.com>
2013-11-08 15:51:23 +0100

    Added list of deployments

--- usr/app/public/js/app.js
+++ usr/app/public/js/app.js
@@ -2,11 +2,11 @@ $(function() {
   $("time").cuteTime();
   $("a.activate-deployment").click(function(event) {
     event.preventDefault();
-    $("div#info").html("<strong>Activating deployment "+$(this).attr("id")+" deployment</strong>. The page will reload automatically when the activation is finished.").show();
+    $("div#info").html("<img src='/deployments/images/spinner.gif'/> <strong>Activating deployment "+$(this).attr("id")+" deployment</strong>. The page will reload automatically when the activation is finished.").show();
     $.post("/deployments/"+$(this).attr('id')+"/activate", function(data) {
       console.log(data);
       $("div#info").hide();
-      setTimeout(function(){location.reload}, 2000);
+      location.reload();
     });
   })
 });
