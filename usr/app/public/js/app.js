68c14ee97b5fc9957fbb7e60830c379c5a456fcf
Michal Fojtik <mfojtik@redhat.com>
2013-11-08 15:37:45 +0100

    Added list of deployments

--- usr/app/public/js/app.js
+++ usr/app/public/js/app.js
@@ -2,8 +2,10 @@ $(function() {
   $("time").cuteTime();
   $("a.activate-deployment").click(function(event) {
     event.preventDefault();
+    $("div#info").html("Activating "+$(this).attr("id")+" deployment...").show();
     $.post("/deployments/"+$(this).attr('id')+"/activate", function(data) {
       console.log(data);
+      $("div#info").hide();
     });
   })
 });
